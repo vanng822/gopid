7dbc96ccd9871499007d5b7abf92e14cc3e031f5
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-01-24 15:03:37 +0100

    Improvement of handling of pid, wait for process to exit completely before writting new pid file

--- gopid.go
+++ gopid.go
@@ -1,18 +1,18 @@
 package gopid
 
 import (
-	"log"
-	"strconv"
-	"os"
-	"syscall"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"syscall"
+	"time"
 )
 
 // Zero means no pid file detected, any other error will cause fatal
 func getPid(pidFile string) int {
 	if _, err := os.Stat(pidFile); err != nil {
-		log.Printf("Could not stat pid file %s", err)
 		return 0
 	}
 	existPidFile, err := os.Open(pidFile)
@@ -24,6 +24,11 @@ func getPid(pidFile string) int {
 	if err != nil {
 		log.Fatalf("Pidfile %s exist but can not read it", pidFile)
 	}
+
+	if len(existPidB) == 0 {
+		return 0
+	}
+
 	existPid, err := strconv.Atoi(fmt.Sprintf("%s", existPidB))
 	if err != nil {
 		log.Fatalf("Could not read from pid file %s, error: %v", pidFile, err)
@@ -31,7 +36,7 @@ func getPid(pidFile string) int {
 	return existPid
 }
 
-// CheckPid will fatal if the pid file exists and not force,
+// CheckPid will fatal if the pid file and process exists and not force,
 // any error when getting pid from this file will also cause fatal.
 // If no pid file live will go on.
 // If there is a pid it will try to kill that process and not fatal
@@ -39,18 +44,32 @@ func getPid(pidFile string) int {
 func CheckPid(pidFile string, force bool) {
 	log.Printf("Checking pid file %s", pidFile)
 	pid := getPid(pidFile)
-	if pid == 0 {
+	if pid <= 0 {
 		return
 	}
+	process, err := os.FindProcess(pid)
+	if err != nil {
+		// if force then we run over it so no fatal here
+		// you have to kill it manually if it is still there
+		log.Printf("Could not find process for pid %d, error: %v", pid, err)
+		return
+	}
+	// there is process
 	if !force {
 		log.Fatalf("Pidfile %s exists with pid %d", pidFile, pid)
 	}
-	if pid > 0 {
-		log.Printf("Try to kill process with pid %d", pid)
-		if err := syscall.Kill(pid, syscall.SIGTERM); err != nil {
-			// if force then we run over it so no fatal here
-			// you have to kill it manually if it is still there
-			log.Printf("Could not kill pid %d, error: %v", pid, err)
+	log.Printf("Try to kill process with pid %d", pid)
+	if err := process.Signal(syscall.SIGTERM); err != nil {
+		log.Printf("Could not kill pid %d, error: %v", pid, err)
+		return
+	}
+	// wait for process to completely terminated
+	if _, err := process.Wait(); err != nil {
+		for i := 0; i < 50; i++ {
+			time.Sleep(5 * time.Millisecond)
+			if _, err := os.FindProcess(pid); err != nil {
+				break
+			}
 		}
 	}
 }
@@ -64,8 +83,8 @@ func CreatePid(pidFile string) {
 	if err != nil {
 		log.Fatalf("Could not create pid file, error: %v", err)
 	}
+	defer pidf.Close()
 	pidf.WriteString(fmt.Sprintf("%d", pid))
-	pidf.Close()
 }
 
 // CleanPid will check first if the current process owns the pid in the file.
