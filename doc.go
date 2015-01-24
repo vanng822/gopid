70ade6de20e9204766d9e28fa5aa24cff8dd29ce
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-01-24 18:02:27 +0100

    Example in doc.go

--- doc.go
+++ doc.go
@@ -1,2 +1,13 @@
 // Package gopid is for checking, creating and cleaning up pid file
+// 
+//	import (
+//		"github.com/vanng822/gopid"
+//	)
+//	func main() {
+//		// code for getting pidFile
+//		gopid.CheckPid(pidFile, force)
+//		gopid.CreatePid(pidFile)
+//		defer gopid.CleanPid(pidFile)
+//		// running your code
+//	}
 package gopid
